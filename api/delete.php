1c002c4e0c25e76142a4444e6c78dbf6f1063ee7
Raag2005 <oskar@raagart.dk>
2022-02-26 18:11:47 +0100

    Update delete.php

--- api/delete.php
+++ api/delete.php
@@ -1,12 +1,16 @@
 
 <?php
-function deleteImage($image){
+function deleteImage($image) {
     include './src/config.php';
-    #connection string
+    // Connection string
     $conn = mysqli_connect($localhost,$dbusername,$dbpassword,$dbname);
-    $sql = "DELETE FROM data WHERE id = $image";
-    $result = mysqli_query($conn,$sql);
-    if($result){
+    
+    // Prevent SQL injection. **Jeg har ikke testet det Lasse**
+    // Source - https://www.php.net/manual/en/mysqli.real-escape-string.php >> Example #1
+    $query = sprintf("DELETE FROM data WHERE id = '%s'", mysqli_real_escape_string($conn, $image)); 
+    $result = mysqli_query($conn,$query);
+    
+    if ($result) {
         return true;
     }else{
         return false;
